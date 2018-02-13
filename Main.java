57791049c4d604961c01f6d19e3cf4287fc9cbc7
Dairo Estiben Beltrán <daiydan99@hotmail.com>
2018-02-13 15:47:03 -0500

    areas y perimetros
    perimetro y area del circulo y el ciadrado

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+Public class Main{
+	Public static void main(String[] arg){
+	int opc;
+	float pi=3.1416; 
+	float acu,pcu,aci,a,b,c,cap;
+	Scanner cap = new Scanner(System.in);
+	System.out.println("Digita la opcion que deses hacer: ");
+	System.out.println("1: area cuadrado");
+	System.out.println("2: area circulo");
+	System.out.println("3: area triangulo");
+		switch(opc){
+			case 1: 
+			System.out.println("Ingresa el lado del cuadrado");
+			a = cap.nextDouble;
+			acu = (a*a);
+			pcu = (a*4); 
+    			System.out.println("El area del cuadrado es: "+acu);
+			System.out.println("El perimetro del cuadrado es: "+pcu);
+			break;
+
+			case 2: 
+			System.out.println("Ingresa el radio del circulo");
+			b = cap.nextDouble;
+			aci = (pi*(b*b));
+    			System.out.println("El area del circulo es: "+aci);
+			System.out.println("El radio del circulo es: "+b);
+			break;
+		}
+	}
+
+}	

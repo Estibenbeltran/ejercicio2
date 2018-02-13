a6391d57a6e674005bfecfaffb0ac0f593c31d57
Dairo Estiben Beltrán <daiydan99@hotmail.com>
2018-02-13 16:01:26 -0500

    areas y perimetros 2
    area y perimetro de cuadrado circulo y triangulo

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ Public class Main{
 	Public static void main(String[] arg){
 	int opc;
 	float pi=3.1416; 
-	float acu,pcu,aci,a,b,c,cap;
+	double acu,pcu,aci,a,b,c,cap,rr,altr,atr,ptr;
 	Scanner cap = new Scanner(System.in);
 	System.out.println("Digita la opcion que deses hacer: ");
 	System.out.println("1: area cuadrado");
@@ -12,7 +12,7 @@ Public class Main{
 		switch(opc){
 			case 1: 
 			System.out.println("Ingresa el lado del cuadrado");
-			a = cap.nextDouble;
+			a = cap.nextDouble();
 			acu = (a*a);
 			pcu = (a*4); 
     			System.out.println("El area del cuadrado es: "+acu);
@@ -21,10 +21,21 @@ Public class Main{
 
 			case 2: 
 			System.out.println("Ingresa el radio del circulo");
-			b = cap.nextDouble;
+			b = cap.nextDouble();
 			aci = (pi*(b*b));
     			System.out.println("El area del circulo es: "+aci);
 			System.out.println("El radio del circulo es: "+b);
+			break;
+
+			case 3:
+			System.out.println("Ingresa el area triangulo");
+			c = cap.nextDouble();
+			rr = Math.sqrt(3);
+			ptr = (c*3);
+			altr = (rr*c)/2;
+			atr = (c*altr)/2;
+			System.out.println("El area del triangulo es: "+atr);
+			System.out.println("El perimetro del triangulo es: "+ptr);
 			break;
 		}
 	}
